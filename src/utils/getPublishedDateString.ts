c26980f7e0ab6005d6143366b3e7facffa3e4966
AlexxanderX <zalexxanderx@gmail.com>
2022-05-08 21:30:40 +0000

    Better date

--- src/utils/getPublishedDateString.ts
+++ src/utils/getPublishedDateString.ts
@@ -2,7 +2,9 @@
 // Own
 import getMonthName from '../utils/getMonthName';
 
+const currentDate = new Date();
+
 export default function getDateString(publishedDateRaw: string): string {
     const publishedDate = new Date(publishedDateRaw);
-    return `${publishedDate.getDate()} ${getMonthName(publishedDate.getMonth())}`;
+    return `${publishedDate.getDate()} ${getMonthName(publishedDate.getMonth())}${currentDate.getFullYear() != publishedDate.getFullYear() ? ` ${publishedDate.getFullYear()}` : ""}`;
 }
