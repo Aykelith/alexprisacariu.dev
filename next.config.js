0ec7a3964391917e51ff48fef37aa940e36c7f23
AlexxanderX <zalexxanderx@gmail.com>
2023-10-16 19:07:41 +0300

    Added the Post 11

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 module.exports = {
+    output: 'export',
+    distDir: 'docs',
     serverRuntimeConfig: {
         PROJECT_ROOT: __dirname,
     },
-    redirects: () => [
-        {
-            source: '/posts/post1_dev_ideas_1_handle_data_between_tabs_components',
-            destination: '/posts/post008_dev_ideas_1_handle_data_between_tabs_components',
-            permanent: true,
-        },
-        {
-            source: '/posts/post2_creating_a_radio_station_on_a_raspberry_pi_using_darkice_and_icecast2',
-            destination: '/posts/post009_creating_a_radio_station_on_a_raspberry_pi_using_darkice_and_icecast2',
-            permanent: true,
-        },
-    ],
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
